e9b3720d7220845e69d3068723576d46b940b112
Todd Albert <todd@bocacode.com>
2023-01-26 12:50:06 -0500

    deployed to cloud function

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+// setup routes
+app.get('/test', (req, res) => {
+  res.send('Holy crap. 💩 We made a cloud function!');
+});
+
+app.get('/test2', (req, res) => {
+  res.send('This other route also works!');
+});
+
+export const api = functions.https.onRequest(app);
